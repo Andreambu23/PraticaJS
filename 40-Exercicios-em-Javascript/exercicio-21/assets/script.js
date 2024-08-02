004c99a9e22e74ceb4b18996cc5d2a9f1dc61d00
André Silva <andreqro234@gmail.com>
2024-08-01 23:43:05 -0300

    Conclusão exercicio 21.

--- 40-Exercicios-em-Javascript/exercicio-21/assets/script.js
+++ 40-Exercicios-em-Javascript/exercicio-21/assets/script.js
@@ -1,13 +1,17 @@
-const multiplicar = (num1, num2) =>{
-  return num1 * num2;
+const calculaDesconto = (precoProduto) => {
+
+  return precoFinal = precoProduto - (precoProduto * (5 / 100))
+
 }
 
-const numero1 = document.getElementById("numero1")
-const numero2 = document.getElementById("numero2")
+const preco = document.getElementById("preco-produto")
+console.log(preco)
+const areaResultado = document.getElementById("resultado")
+console.log(areaResultado);
 
-document.getElementById("multiplicar").onclick = () =>{
-  const multiplicacao = multiplicar(parseInt(numero1.value), parseInt(numero2.value))
-
-  document.getElementById("resultado").innerHTML = multiplicacao
-
+const calcularDesconto = document.getElementById("calcula-desconto").onclick = () => {
+  const precoProduto = parseInt(preco.value)
+  const precoFinal = calculaDesconto(precoProduto)
+  areaResultado.innerHTML = precoFinal
+  preco.value = ""
 }
